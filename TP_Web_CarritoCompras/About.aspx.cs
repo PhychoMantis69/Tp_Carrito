f01e8e6acffebdd18ac1eec5464244d25db62a25
lgomezcaballero <leandrogomez343@gmail.com>
2022-05-29 18:05:40 -0300

    Se agregan barra de busqueda y divs para articulos(cards)

--- TP_Web_CarritoCompras/About.aspx.cs
+++ TP_Web_CarritoCompras/About.aspx.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TP_Web_CarritoCompras
 {
     public partial class About : Page
     {
+        //ArticuloNegocio articulos = new ArticuloNegocio();
         protected void Page_Load(object sender, EventArgs e)
         {
 
--- TP_Web_CarritoCompras/Default.aspx.cs
+++ TP_Web_CarritoCompras/Default.aspx.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TP_Web_CarritoCompras
 {
     public partial class _Default : Page
     {
+        public List<Articulo> listado{ get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ArticuloNegocio articulo = new ArticuloNegocio();
+            listado = new List<Articulo>();
+            listado = articulo.listar();
         }
     }
 }
